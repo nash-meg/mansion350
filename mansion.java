2df963323d7b5a118d0299d6fdbe0ae129a3f080
Meghan <nashmeg@mail.gvsu.edu>
2021-11-11 14:08:21 -0500

    mansion format

--- mansion.java
+++ mansion.java
@@ -1,29 +1,46 @@
 public class mansion {
 
+    /*********** GENERAL FORMAT *************/
+        //println() the story
+            //kdnlfnw w elfnwf wkefiqw fwefwr hqthi5jt34 v dclvknerfnmqwd
+        //new println() for the quesitons
+            //do you want to:
+        //new println() for the options
+            //1 - pqfnqpfn
+            //2 - somfoefn
+            //3 - wqfnmefn
+        //println() tell user to type in
+            //Type the number of the option you choose or press m for map
+        //scan the keyboard input
+        //store in history
+        //if statements to generate where to go next
+        //set any inventory
+
+    /*********** PROLOGUE *************/
     //prologue method - mostly text, there are options but they don't affect anything
-        //show choices in history?
+        //show choices in history if time?
 
     //floor methods
         //put all the room methods on that floor in their respective floor method
         //call the GUI for each floor in the methods
 
-    //floor 1
+    /*********** FLOOR ONE *************/
         //room 1 - your bedroom
         //room 5a - hallway (a bc floor 1)
         //room 4 - mallory's bedroom
 
-    //floor 2
+    /*********** FLOOR TWO *************/
         //room 5b - hallway (b bc floor 1)
         //room 9 - chamber (butler fight scene)
         //room 10 - chamber (potion room)
         //room 8 - hallway/stairway (no method for bathroom, just display)
 
-    //floor 3
+    /*********** FLOOR THREE *************/
         //room 11 - kitchen
         //room 7 - greenhouse (not labeled on correct floor on pic)
         //hide from old man - crosses rooms, lots of booleans for if potion or spell book
 
-    //cellar (floor 4)
+    /*********** CELLAR *************/
         //general (if not matches or pet cat)
         //if matches
         //if pet cat
