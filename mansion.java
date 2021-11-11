3240ebb5c437bc8e28a3330cfdf8c0d42984f763
Meghan <nashmeg@mail.gvsu.edu>
2021-11-11 12:45:03 -0500

    Comments for plan in each class

--- mansion.java
+++ mansion.java
@@ -1,5 +1,32 @@
 public class mansion {
 
+    //prologue method - mostly text, there are options but they don't affect anything
+        //show choices in history?
+
+    //floor methods
+        //put all the room methods on that floor in their respective floor method
+        //call the GUI for each floor in the methods
+
+    //floor 1
+        //room 1 - your bedroom
+        //room 5a - hallway (a bc floor 1)
+        //room 4 - mallory's bedroom
+
+    //floor 2
+        //room 5b - hallway (b bc floor 1)
+        //room 9 - chamber (butler fight scene)
+        //room 10 - chamber (potion room)
+        //room 8 - hallway/stairway (no method for bathroom, just display)
+
+    //floor 3
+        //room 11 - kitchen
+        //room 7 - greenhouse (not labeled on correct floor on pic)
+        //hide from old man - crosses rooms, lots of booleans for if potion or spell book
+
+    //cellar (floor 4)
+        //general (if not matches or pet cat)
+        //if matches
+        //if pet cat
 
 
 }
--- player.java
+++ player.java
@@ -1,5 +1,14 @@
 public class player {
 
+    //need to add a login: user, pass, and history of previous attempts
+        //need to link history with the choice method
+        //make history method last, choice method is just numbers (in same order each time), maybe arrayList,
+            //maybe pop up as a text file after the end to see what you did
+    //getters and setters for inventory
+        //shoes, three booleans for cat, matches, coat, book, purple potion, letter opener
+    //choice method: equals next keyboard input, var named choice, adds choice to arrayList then returns
+
+
     public static void main (String[] args){
 
     }
