14fa3cf16f4d9c7d8e58c214f8940218ebc2a489
Meghan <nashmeg@mail.gvsu.edu>
2021-11-16 15:38:11 -0500

    mansion template

--- mansion.java
+++ mansion.java
@@ -1,24 +1,27 @@
-
+import java.util.Scanner;
 
 public class mansion {
+    private int choice;
+
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("enter which option you choose");
+    choice = keyboard.nextInt();
 
     /*********** GENERAL FORMAT *************/
-        //println() the story
-
-
-            //kdnlfnw w elfnwf wkefiqw fwefwr hqthi5jt34 v dclvknerfnmqwd
-        //new println() for the quesitons
-            //do you want to:
-        //new println() for the options
-            //1 - pqfnqpfn
-            //2 - somfoefn
-            //3 - wqfnmefn
-        //println() tell user to type in
-            //Type the number of the option you choose or press m for map
-        //scan the keyboard input
-        //store in history
-        //if statements to generate where to go next
-        //set any inventory
+    //println() the story
+        //kdnlfnw w elfnwf wkefiqw fwefwr hqthi5jt34 v dclvknerfnmqwd
+    //new println() for the quesitons
+        //do you want to:
+    //new println() for the options
+        //1 - pqfnqpfn
+        //2 - somfoefn
+        //3 - wqfnmefn
+    //println() tell user to type in
+        //Type the number of the option you choose or press m for map
+    //scan the keyboard input
+    //store in history
+    //if statements to generate where to go next
+    //set any inventory
 
     /*********** PROLOGUE *************/
     //prologue method - mostly text, there are options but they don't affect anything
@@ -29,25 +32,25 @@ public class mansion {
         //call the GUI for each floor in the methods
 
     /*********** FLOOR ONE *************/
-        //room 1 - your bedroom
-        //room 5a - hallway (a bc floor 1)
-        //room 4 - mallory's bedroom
+    //room 1 - your bedroom
+    //room 5a - hallway (a bc floor 1)
+    //room 4 - mallory's bedroom
 
     /*********** FLOOR TWO *************/
-        //room 5b - hallway (b bc floor 1)
-        //room 9 - chamber (butler fight scene)
-        //room 10 - chamber (potion room)
-        //room 8 - hallway/stairway (no method for bathroom, just display)
+    //room 5b - hallway (b bc floor 1)
+    //room 9 - chamber (butler fight scene)
+    //room 10 - chamber (potion room)
+    //room 8 - hallway/stairway (no method for bathroom, just display)
 
     /*********** FLOOR THREE *************/
-        //room 11 - kitchen
-        //room 7 - greenhouse (not labeled on correct floor on pic)
-        //hide from old man - crosses rooms, lots of booleans for if potion or spell book
+    //room 11 - kitchen
+    //room 7 - greenhouse (not labeled on correct floor on pic)
+    //hide from old man - crosses rooms, lots of booleans for if potion or spell book
 
     /*********** CELLAR *************/
-        //general (if not matches or pet cat)
-        //if matches
-        //if pet cat
+    //general (if not matches or pet cat)
+    //if matches
+    //if pet cat
 
 
 }
