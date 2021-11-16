33b21bf6100790668f3b45ca434429288dc71197
Meghan <nashmeg@mail.gvsu.edu>
2021-11-16 16:37:35 -0500

    directions for how to run the game in main

--- main.java
+++ main.java
@@ -1,13 +1,27 @@
 import java.util.Scanner;
 
 public class main {
-    //this is the main :)
     public static void main(String[] args) {
-
-        player  mansion = new player();
-        System.out.print("please");
+        player mansion = new player();
+        //call directions
+        //call prologue (mansion.class)
+        //call floor one method (mansion.class)
+            //call room1 method
+            //call room4 if chose to go in
+            //call room5a to go to floor two
+        //call floor two method (mansion.class)
+            //call 5b to start
+            //call 9
+            //call 10
+            //call 5b and 8?
+        //call floor three method (mansion.class)
+            //call 11
+            //call 7 if needed
+            //call hideFromMan
+        //call cellar method (mansion.class)
+            //call inventory to see which death
+            //call appropriate death/winning cellar method
     }
-
 }
 
 
