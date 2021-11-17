ec3cb36e22a5df9408988a4f054fc4fbe569dfcc
Meghan <nashmeg@mail.gvsu.edu>
2021-11-17 15:14:50 -0500

    adding methods

--- mansion.java
+++ mansion.java
@@ -29,44 +29,47 @@ public class mansion {
         //call setters from player.class
 
     /*********** DIRECTIONS *************/
-    System.out.println("Sinister Happenings at Huxley Manor\n" +
-            "This game contains themes gore, violence, and manipulation/mind control.\n" +
-            "Enter at your own risk...\n" +
-            "Before goosebumps plague your arms and your eyes cannot be ripped away from the screen," +
-            "here are some things to know about our spooky little game:\n" +
-            "This is a text-based game, so there are very little graphics. If you don’t like reading," +
-            "sorry bud, this isn’t the game for you. We want to let our words guide your imagination," +
-            "and we figure you know how to scare yourself better than we ever could.\n" +
-            "This is also a choose-your-own-adventure game, so you will read the story and choose from" +
-            "a handful of actions at specific moments to determine what happens next. Some of these" +
-            "options will allow you to continue through the escape, but some may result in death. Choose" +
-            "wisely and you might make it out alive.\n" +
-            "To select an option, type the number on your keyboard and hit enter. Also, some sections of" +
-            "the story are longer than others, so you may need to hit enter to continue reading.\n" +
-            "The one graphic you do get to reap the benefits of is a map. You can pull up this map during" +
-            "given opportunities by typing “m” and hitting enter.\n" +
-            "We hope you enjoy, good luck, and don’t kick the cat.\n" +
-            "- The devious minds behind the code");
-    System.out.println("Please choose an option below:");
-    System.out.println("1 - Continue to game.\n" +
-            "2 - Exit.");
-    //call choice method in player.class
-    //some stuff here
+    public void directions() {
+        System.out.println("Sinister Happenings at Huxley Manor\n" +
+                "This game contains themes gore, violence, and manipulation/mind control.\n" +
+                "Enter at your own risk...\n" +
+                "Before goosebumps plague your arms and your eyes cannot be ripped away from the screen," +
+                "here are some things to know about our spooky little game:\n" +
+                "This is a text-based game, so there are very little graphics. If you don’t like reading," +
+                "sorry bud, this isn’t the game for you. We want to let our words guide your imagination," +
+                "and we figure you know how to scare yourself better than we ever could.\n" +
+                "This is also a choose-your-own-adventure game, so you will read the story and choose from" +
+                "a handful of actions at specific moments to determine what happens next. Some of these" +
+                "options will allow you to continue through the escape, but some may result in death. Choose" +
+                "wisely and you might make it out alive.\n" +
+                "To select an option, type the number on your keyboard and hit enter. Also, some sections of" +
+                "the story are longer than others, so you may need to hit enter to continue reading.\n" +
+                "The one graphic you do get to reap the benefits of is a map. You can pull up this map during" +
+                "given opportunities by typing “m” and hitting enter.\n" +
+                "We hope you enjoy, good luck, and don’t kick the cat.\n" +
+                "- The devious minds behind the code");
+        System.out.println("Please choose an option below:");
+        System.out.println("1 - Continue to game.\n" +
+                "2 - Exit.");
+        //call choice method in player.class
+    }
 
     /*********** PROLOGUE *************/
     //prologue method - mostly text, there are options but they don't affect anything
         //show choices in history if time?
-     System.out.println("story goes here" +
-             "and here");
-     System.out.println("Please choose an option below:");
-     System.out.println("1 - choice.\n" +
-            "2 - choice.");
-    //call choice method in player.class
+    public void prologue() {
+        System.out.println("story goes here" +
+                "and here");
+        System.out.println("Please choose an option below:");
+        System.out.println("1 - choice.\n" +
+                "2 - choice.");
+        //call choice method in player.class
+    }
 
     /*********** FLOOR ONE *************/
     //room 1 - your bedroom
     /*
-    public room1 (){
+    public void room1 (){
         System.out.println("story goes here" +
              "and here");
         System.out.println("Please choose an option below:");
@@ -78,7 +81,7 @@ public class mansion {
 
     //room 5a - hallway (a bc floor 1)
     /*
-    public room5a (){
+    public void room5a (){
         System.out.println("story goes here" +
              "and here");
         System.out.println("Please choose an option below:");
@@ -90,7 +93,7 @@ public class mansion {
 
     //room 4 - mallory's bedroom
     /*
-    public room4 (){
+    public void room4 (){
         System.out.println("story goes here" +
              "and here");
         System.out.println("Please choose an option below:");
@@ -101,7 +104,7 @@ public class mansion {
      */
 
     /*********** FLOOR TWO *************/
-    //room 5b - hallway (b bc floor 1)
+    //room void 5b - hallway (b bc floor 1)
     /*
     public room5b (){
         System.out.println("story goes here" +
@@ -115,7 +118,7 @@ public class mansion {
 
     //room 9 - chamber (butler fight scene)
     /*
-    public room9 (){
+    public void room9 (){
         System.out.println("story goes here" +
              "and here");
         System.out.println("Please choose an option below:");
@@ -127,7 +130,7 @@ public class mansion {
 
     //room 10 - chamber (potion room)
     /*
-    public room10 (){
+    public void room10 (){
         System.out.println("story goes here" +
              "and here");
         System.out.println("Please choose an option below:");
@@ -139,7 +142,7 @@ public class mansion {
 
     //room 8 - hallway/stairway (no method for bathroom, just display)
     /*
-    public room8 (){
+    public void room8 (){
         System.out.println("story goes here" +
              "and here");
         System.out.println("Please choose an option below:");
@@ -152,7 +155,7 @@ public class mansion {
     /*********** FLOOR THREE *************/
     //room 11 - kitchen
     /*
-    public room11 (){
+    public void room11 (){
         System.out.println("story goes here" +
              "and here");
         System.out.println("Please choose an option below:");
@@ -164,7 +167,7 @@ public class mansion {
 
     //room 7 - greenhouse (not labeled on correct floor on pic)
     /*
-    public room7 (){
+    public void room7 (){
         System.out.println("story goes here" +
              "and here");
         System.out.println("Please choose an option below:");
@@ -176,7 +179,7 @@ public class mansion {
 
     //hide from old man - crosses rooms, lots of booleans for if potion or spell book
     /*
-    public hideFromMan (){
+    public void hideFromMan (){
         System.out.println("story goes here" +
              "and here");
         System.out.println("Please choose an option below:");
@@ -189,7 +192,7 @@ public class mansion {
     /*********** CELLAR *************/
     //general (if not matches or pet cat)
     /*
-    public generalCellar (){
+    public void generalCellar (){
         System.out.println("story goes here" +
              "and here");
         System.out.println("Please choose an option below:");
@@ -201,7 +204,7 @@ public class mansion {
 
     //if matches
     /*
-    public matchCellar (){
+    public void matchCellar (){
         System.out.println("story goes here" +
              "and here");
         System.out.println("Please choose an option below:");
@@ -213,7 +216,7 @@ public class mansion {
 
     //if pet cat
     /*
-    public petCatCellar (){
+    public void petCatCellar (){
         System.out.println("story goes here" +
              "and here");
         System.out.println("Please choose an option below:");
